d2fdbe0a70b2185434046ca485d49799c6092960
Dmitry <vip.mihaslw@mail>
2022-09-03 20:37:25 +0300

    домашняя3

---
+++ Task19/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 19: Напишите программу, которая принимает на
+//  вход пятизначное число 
+//  и проверяет, 
+//  является ли оно палиндромом.
+Console.WriteLine("Введите целое, пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine()),result = 0;
+int square=(num); 
+int a = num % 10;
+num = num / 10;
+result += a * 10000;
+
+a = num % 10;
+num = num / 10;
+result += a * 1000;
+
+a = num % 10;
+num = num / 10;
+result += a * 100;
+
+a = num % 10;
+num = num / 10;
+result += a * 10;
+
+a = num % 10;
+num = num / 10;
+result += a;
+if (square==result) Console.WriteLine($"число {square} - палиндром");
+else Console.WriteLine($"число {square} - не палиндром");
+
---
+++ Task21/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
+
+// Напишите программу которая на вход принимает координаты 
+// двух точек и выдает расстояние между ними в 3д пространстве
+
+Console.WriteLine("Введите координаты первой точки");
+Console.Write("x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double Pifagor(int xc1, int yc1, int zc1 ,int xc2, int yc2, int zc2)
+{
+    return Math.Sqrt((xc2-xc1)*(xc2-xc1) + (yc2-yc1)*(yc2-yc1) + (zc2-zc1)*(zc2-zc1));    
+}
+double result = Pifagor(x1, y1, z1, x2, y2, z2);
+Console.WriteLine($"длина между точками в пространстве 3d это " +Math.Round(result, 2));
+
